11f4ef9c3913afe26ca0f9c2bdf04bf3849ace0a
Vince Elizaga <vncelizaga@gmail.com>
2016-10-27 19:19:54 +0800

    refactor nexmo and mailgunjs api

--- cloud/main.js
+++ cloud/main.js
@@ -1,11 +1,3 @@
-var Nexmo = require('nexmo');
-
-var nexmo = new Nexmo({
-  apiKey: '2c909569', 
-  apiSecret: '80c3b6a1fed15b33'
-}
-);
-
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
@@ -13,10 +5,14 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.afterSave("Booking", function(request) {
     console.log(request.object.attributes);
     var booking = request.object.attributes;
+    
     if(booking.artistInfo.contactNumber){
-        var schedule = booking.schedule.toString();     
-        nexmo.message.sendSms('639272326087', booking.artistInfo.contactNumber, 'Hi ' + booking.artistInfo.firstName +',\n\nNew Booking From ' + booking.customerInfo.firstName + ' ' + booking.customerInfo.lastName + ' - Schedule: ' + schedule + ', Total Bill: ' + booking.totalBill +'\n\nPlease check your account, to accept/reject the booking. Thank You! [BLUSH-PH]', function(){
-            console.log('sent')
-        });              
-    }  
+        var nexmo = require('../notificationAPI/nexmo.js');
+        nexmo.send(booking);          
+    }
+
+    if(booking.artistInfo.email){
+        var mailgun = require('../notificationAPI/mailgun.js');
+        mailgun.send(booking);                    
+    }          
 });
---
+++ notificationAPI/mailgun.js
@@ -0,0 +1,22 @@
+function send(booking){
+    var api_key = 'key-67b86d7ad8fdbd8b29156d4d246a5731';
+    var domain = 'sandboxa20b47392fdb41d18b3629e330882f57.mailgun.org';
+    var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+    var data = {
+        from: 'BlushPH <blushphilippines@gmail.com>',
+        to: booking.artistInfo.email,
+        subject: 'BlushPH - New Booking From ' + booking.customerInfo.firstName + ' ' + booking.customerInfo.lastName,
+        text: 'Hi ' + booking.artistInfo.firstName +',\n\nNew Booking From ' + booking.customerInfo.firstName + ' ' + booking.customerInfo.lastName + ' - Schedule: ' + schedule + ', Total Bill: ' + booking.totalBill +'\n\nPlease check your account, to accept/reject the booking. Thank You! [BLUSH-PH]'
+    };
+
+    mailgun.messages().send(data, function (error, body) {
+        console.log(body);
+    });    
+}
+
+module.exports = {
+    send : send
+}
+
+  
